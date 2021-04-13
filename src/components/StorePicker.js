2a4dde51f3d705c5595cd668dc053ec1fb41ac19
Craig <40456374@live.napier.ac.uk>
2021-04-13 14:53:52 +0100

    adds css file to index.js file

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -7,7 +7,7 @@ class StorePicker extends React.Component {
 				<h2>Please Enter A Store</h2>
 				<input type="text" required placeholder="Store Name" />
 				<button type="submit">Visit Store</button>
-			</form>)
+			</form>);
 	}
 }
 export default StorePicker;	
